1718a4c8f55a371d8c346a65637176d72c9562c3
Surain <surain.wira@gmail.com>
2023-06-29 12:40:15 +0800

    added delete API calls

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,6 +1,6 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux"
-import { publicRequest } from "../requestMethods"
-import { getProductStart, getProductSuccess, getProductFail } from "./productRedux";
+import { publicRequest, userRequest } from "../requestMethods"
+import { getProductStart, getProductSuccess, getProductFail, deleteProductStart, deleteProductSuccess, deleteProductFail } from "./productRedux";
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
@@ -15,5 +15,14 @@ export const getProducts = async (dispatch) => {
     try {
         const res = await publicRequest.get("/products");
         dispatch(getProductSuccess(res.data))
-    } catch (err) { dispatch(getProductFail()) }
+    } catch (err) { dispatch(getProductFail(err.message)) }
+}
+
+
+export const deleteProducts = async (id, dispatch) => {
+    dispatch(deleteProductStart());
+    try {
+        const res = await userRequest.delete(`products/${id}`);
+        dispatch(deleteProductSuccess(id))
+    } catch (err) { dispatch(deleteProductFail(err.message)) }
 }
