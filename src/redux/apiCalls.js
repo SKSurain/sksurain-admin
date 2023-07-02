99e2e895aa936c5cd7c58035b9f5b4932c43c27b
Surain <surain.wira@gmail.com>
2023-07-02 16:04:53 +0800

    added update and add product

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,6 +1,19 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux"
 import { publicRequest, userRequest } from "../requestMethods"
-import { getProductStart, getProductSuccess, getProductFail, deleteProductStart, deleteProductSuccess, deleteProductFail } from "./productRedux";
+import {
+    getProductStart,
+    getProductSuccess,
+    getProductFail,
+    deleteProductStart,
+    deleteProductSuccess,
+    deleteProductFail,
+    updateProductSuccess,
+    updateProductStart,
+    updateProductFail,
+    addProductStart,
+    addProductSuccess,
+    addProductFail
+} from "./productRedux";
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
@@ -25,4 +38,20 @@ export const deleteProducts = async (id, dispatch) => {
         const res = await userRequest.delete(`products/${id}`);
         dispatch(deleteProductSuccess(id))
     } catch (err) { dispatch(deleteProductFail(err.message)) }
+}
+
+export const updateProducts = async (id, product, dispatch) => {
+    dispatch(updateProductStart());
+    try {
+        const res = await userRequest.put(`products/${id}`);
+        dispatch(updateProductSuccess({ id: id, product: product }))
+    } catch (err) { dispatch(updateProductFail(err.message)) }
+}
+
+export const addProducts = async (product, dispatch) => {
+    dispatch(addProductStart());
+    try {
+        const res = await userRequest.post(`products`, product);
+        dispatch(addProductSuccess(res.data))
+    } catch (err) { dispatch(addProductFail(err.message)) }
 }
--- src/redux/productRedux.js
+++ src/redux/productRedux.js
@@ -36,9 +36,47 @@ const productSlice = createSlice({
         deleteProductFail: (state, action) => {
             state.isFetching = false
             state.error = action.payload
+        }, //UPDATE PRODUCTS
+        updateProductStart: (state) => {
+            state.isFetching = true
+            state.error = false
+        },
+        updateProductSuccess: (state, action) => {
+            state.isFetching = false
+            state.products[state.products.findIndex((item) =>
+                item._id === action.payload.id)] =
+                action.payload.product;
+        },
+        updateProductFail: (state, action) => {
+            state.isFetching = false
+            state.error = action.payload
+        },
+        //ADD PRODUCTS
+        addProductStart: (state) => {
+            state.isFetching = true
+            state.error = false
+        },
+        addProductSuccess: (state, action) => {
+            state.isFetching = false
+            state.products.push(action.payload)
+        },
+        addProductFail: (state, action) => {
+            state.isFetching = false
+            state.error = action.payload
         }
     }
 })
 
-export const { getProductStart, getProductSuccess, getProductFail, deleteProductStart, deleteProductSuccess, deleteProductFail } = productSlice.actions
+export const { getProductStart,
+    getProductSuccess,
+    getProductFail,
+    deleteProductStart,
+    deleteProductSuccess,
+    deleteProductFail,
+    updateProductStart,
+    updateProductSuccess,
+    updateProductFail,
+    addProductStart,
+    addProductSuccess,
+    addProductFail } = productSlice.actions
 export default productSlice.reducer; 
