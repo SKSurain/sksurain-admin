d1f3c1b11573f7851a0336718e716a2855ea5234
Surain <surain.wira@gmail.com>
2023-07-02 16:03:20 +0800

    record form data and upload to firebase and mongo

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,30 +1,97 @@
+import { useState } from "react";
 import "./newProduct.css";
+import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { addProducts } from "../../redux/apiCalls"
+import app from '../../firebase'
+import { useDispatch } from 'react-redux'
 
 export default function NewProduct() {
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [categories, setCategories] = useState([]);
+  const dispatch = useDispatch()
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+  const handleCategories = (e) => {
+    setCategories(e.target.value.split(","))
+  }
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName)
+    //UPLOAD FILE AND CALL API
+    const uploadTask = uploadBytesResumable(storageRef, file)
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done")
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+        }
+      },
+      (error) => {
+        console.log(error)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const product = { ...inputs, img: downloadURL, categories: categories };
+          addProducts(product, dispatch)
+        })
+      }
+    )
+  }
+
+  console.log(file)
+
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input type="file" name="img" id="file" onChange={e => setFile(e.target.files[0])} />
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>Title</label>
+          <input name="title" type="text" placeholder="title" onChange={handleChange} />
         </div>
         <div className="addProductItem">
-          <label>Stock</label>
-          <input type="text" placeholder="123" />
+          <label>Description</label>
+          <input name="description" type="text" placeholder="description" onChange={handleChange} />
         </div>
         <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <label>Price</label>
+          <input type="number" name="price" placeholder="price" onChange={handleChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input type="text" name="categories" placeholder="sunglasses,watches" onChange={handleCategories} />
+        </div>
+        <div className="addProductItem">
+          <label>In Stock</label>
+          <select onChange={handleChange} name="inStock">
+            <option value="true">Yes</option>
+            <option value="false">No</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+        <button className="addProductButton" onClick={handleClick}>Create</button>
       </form>
     </div>
   );
