234060ce086b1858a7697b728b33ae8fd5a41a33
Surain <surain.wira@gmail.com>
2023-07-02 16:02:14 +0800

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAKE5g3q_dwfkewlEDfx_5KdobuZWpAmh4",
+    authDomain: "bondo-clothing.firebaseapp.com",
+    projectId: "bondo-clothing",
+    storageBucket: "bondo-clothing.appspot.com",
+    messagingSenderId: "558479685631",
+    appId: "1:558479685631:web:2b5709ea071a36105b19ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
